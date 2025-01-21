8dbb4389caf651313bd4004dd4ee6444abde32b2
Merthon <imchenxing42@gmail>
2025-01-21 23:05:06 +0800

    fix methods

--- BasicProjects/bs4/douban.py
+++ BasicProjects/bs4/douban.py
@@ -4,7 +4,8 @@ import json
 
 base_url = "https://movie.douban.com/top250"
 headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36\
+                        (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
     }
 
 movies = []
